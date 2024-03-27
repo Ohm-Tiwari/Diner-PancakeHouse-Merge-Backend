cb9bcc8358ef6b7f9f1758da77af7f676ae08f13
Ohm-Tiwari <otiwari24@gmail.com>
2024-03-27 08:24:38 -0400

    made changes

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,17 +1,15 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
-
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.CafeRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -23,23 +21,29 @@ public class MergerController {
     DinerRepository dinerRepository;
     PancakeHouseRepository pancakeHouseRepository;
 
-    public MergerController(DinerController dinerController, PancakeHouseController pancakeHouseController){
+    public MergerController(DinerRepository dinerRepository, PancakeHouseRepository pancakeHouseRepository) {
         this.dinerRepository = dinerRepository;
         this.pancakeHouseRepository = pancakeHouseRepository;
     }
 
     @GetMapping
     public List<MenuItem> get() {
-         List<MenuItem> menuItems = new ArrayList<>();
-         Iterator<MenuItem> lunchItems = dinerRepository.getTheMenuIterator();
-         while(lunchItems.hasNext()){
-             menuItems.add(lunchItems.next());
-         }
-         List<MenuItem> breakfastItems = pancakeHouseRepository.getTheMenuIterator();
-         while(breakfastItems.hasNext()){
+        List<MenuItem> menuItems = new ArrayList<>();
+        Iterator<MenuItem> lunchItems = dinerRepository.getTheMenuIterator();
+        while(lunchItems.hasNext()) {
+            menuItems.add(lunchItems.next());
+        }
+
+        Iterator<MenuItem> breakfastItems = pancakeHouseRepository.getTheMenuIterator();
+        while(breakfastItems.hasNext()) {
             menuItems.add(breakfastItems.next());
         }
+
+        Iterator<MenuItem> cafeItems = CafeRepository.getTheMenuIterator();
+        while(cafeItems.hasNext()) {
+            menuItems.add(cafeItems.next());
+        }
+
         return menuItems;
     }
-
 }
--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/DinerMenuIterator.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/DinerMenuIterator.java
@@ -1,45 +1,41 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.model;
 
-import java.awt.*;
 import java.util.Iterator;
 
-public class DinerMenuIterator implements Iterator {
-
-    MenuItem[] MenuItems;
-
+public class DinerMenuIterator implements Iterator<MenuItem> {
+    MenuItem[] menuItems;
     int position = 0;
 
-    public DinerMenuIterator(MenuItem[] menuItem){ this.MenuItems = MenuItems;}
+    public DinerMenuIterator(MenuItem[] menuItems) {
+        this.menuItems = menuItems;
+    }
 
-    public String next() {
-        String menuItem = MenuItems[position];
-        position = position + 1;
+    @Override
+    public boolean hasNext() {
+        if(position >= menuItems.length || (menuItems[position] == null)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public MenuItem next() {
+        MenuItem menuItem = menuItems[position];
+        position += 1;
         return menuItem;
     }
 
-    public boolean hasNext() {
-        if (position >= MenuItems.length || MenuItems[position] == null) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-
     @Override
-    public void remove(){
-        if(position <= 0){
-            throw new IllegalArgumentException("Cannot remove item");
+    public void remove() {
+        if (position <= 0) {
+            throw new IllegalStateException
+                    ("You can't remove an item until you've done at least one next()");
         }
-        if(MenuItems[position - 1] != null){
-            for (int i = position - 1; i < (MenuItems.length - 1); i++) {
-                MenuItems[i] = MenuItems[i + 1];
+        if (menuItems[position-1] != null) {
+            for (int i = position-1; i < (menuItems.length-1); i++) {
+                menuItems[i] = menuItems[i+1];
             }
-            MenuItems[MenuItems.length - 1] = null;
-
+            menuItems[menuItems.length-1] = null;
         }
     }
-
-
-
 }
